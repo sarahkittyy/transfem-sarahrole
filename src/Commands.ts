5aa337acedd3e202801d2f69e3906c2fc1495fb8
sarahkittyy <chrisohlin1@gmail.com>
2019-04-26 08:41:08 -0400

    added options & changed callback params

--- src/Commands.ts
+++ src/Commands.ts
@@ -12,7 +12,12 @@ export interface Command
 {
 	desc: string;
 	args?: Array<CommandArg>;
-	callback: (err: Error | null, ...args: any[]) => void;
+	callback: (err: Error | null, msg: Discord.Message, ...args: any[]) => void;
+};
+
+interface CommandOpts
+{
+	prefix: string;
 };
 
 /**
@@ -25,12 +30,33 @@ export default class Commands
 	 */
 	private commands: Map<string, Command>;
 	
+	private opts: CommandOpts;
+	
 	/**
-	 * The bot constructor.
+	 * The commands constructor.
 	 */
 	public constructor()
 	{
+		//Init everyhting.
 		this.commands = new Map<string, Command>();
+		this.opts = {
+			prefix: '~$'
+		};
+	}
+	
+	/**
+	 * Get/Set the option.
+	 */
+	public opt(opt: string, value?: any): any
+	{
+		if(value)
+		{
+			this.opts[opt] = value;
+		}
+		else
+		{
+			return this.opts[opt];
+		}
 	}
 	
 	/**
@@ -44,35 +70,47 @@ export default class Commands
 	/**
 	 * Run the given command.
 	 */
-	public runCommand(name: string, ...args: any[]): void
+	public runCommand(msg: Discord.Message, name: string, ...args: any[]): boolean
 	{
 		//Grab the command.
 		let cmd: Command = this.commands.get(name);
+		if(!cmd)
+		{
+			return false;
+		}
 		
 		let newArgs: any[] = [];
 		
 		//Iterate over all args.
 		//If we run out of args in the args[] array,
 		//Substitute in default args.
-		cmd.args.forEach((value, index)=>{
-			let given = args[index];
-			if(given) //If it was given...
-			{	
-				newArgs.push(given);
-			}
-			else if (value.default) //Otherwise, if there's a default value..
-			{
-				newArgs.push(value.default);
-			}
-			else //Otherwise..
-			{
-				//Error.
-				cmd.callback(new Error('Invalid Args.'));
-			}
-		});
+		if(cmd.args)
+		{
+			cmd.args.forEach((value, index)=>{
+				let given = args[index];
+				if(given) //If it was given...
+				{	
+					newArgs.push(given);
+				}
+				else if (value.default) //Otherwise, if there's a default value..
+				{
+					newArgs.push(value.default);
+				}
+				else //Otherwise..
+				{
+					//Error.
+					cmd.callback(new Error('Invalid Args.'), msg);
+				}
+			});
+		}
+		else
+		{
+			newArgs = args;
+		}
 		
 		//Run the callback with the given args.
-		cmd.callback(null, ...newArgs);
+		cmd.callback(null, msg, ...newArgs);
+		return true;
 	}
 	
 };
